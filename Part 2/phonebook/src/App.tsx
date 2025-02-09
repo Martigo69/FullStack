e6b74176797e41012703a95c6b4dcef30ad2dd2e
Mrityunjaya <mrityunjayamak@gmail.com>
2025-02-09 20:21:05 +0100

    part2 d

--- Part 2/phonebook/src/App.tsx
+++ Part 2/phonebook/src/App.tsx
@@ -1,42 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import phoneService from "./services/Phoneservice"
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchPerson, setNewSearchPerson] = useState('')
 
-  const personsData = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
+  useEffect(() => {
+    phoneService.getPersons()
+      .then(persons => {
+        setPersons(persons)
+      })
+  }, [])
 
-const [newName, setNewName] = useState('')
-const [newNumber, setNewNumber] = useState('')
-const [searchPerson, setNewSearchPerson] = useState('')
 
-const addNewPerson = (event) => {
+  const addNewPerson = (event) => {
     event.preventDefault()
     const newNameObject = {
-      id: persons.length + 1,
       name: newName,
       number: newNumber
     }
-    const searchPerson = persons.filter(person => person.name === newNameObject.name)
-    if(searchPerson.length != 0){
-      alert(`${newNameObject.name} is already added to phonebook`)
+    const searchPerson = persons.filter(person => person.name === newNameObject.name)[0]
+    if(searchPerson != null){
+      const resp = window.confirm(`${newNameObject.name} is already added to phonebook, replace the old number with the new one?`)
+      if (resp) {
+      phoneService.updatePerson(searchPerson.id,newNameObject)
+      .then(returnedPerson => {
+        setPersons(persons.map(person => searchPerson.id == person.id ? returnedPerson : person ))
+        setNewName('')
+        setNewNumber('')
+        })
+      } else {
+        setNewName('')
+        setNewNumber('')
+      }
     } else {
-      setPersons(persons.concat(newNameObject))
-      setNewName('')
-      setNewNumber('')
+      phoneService.createPerson(newNameObject)
+      .then(person => {
+        setPersons(persons.concat(person))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   } 
 
@@ -50,11 +59,7 @@ const addNewPerson = (event) => {
 
   const setNewSearch = (event) => {
     setNewSearchPerson(event.target.value)
-    const filteredPersons = personsData.filter(personData => personData.name.toLowerCase().includes(event.target.value.toLowerCase()))
-    setPersons(filteredPersons)
-    setNewName('')
-    setNewNumber('')
- }
+  }
   
   return (
     <div>
@@ -68,7 +73,7 @@ const addNewPerson = (event) => {
                   addNewPerson={addNewPerson}
                   />
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} searchPerson={searchPerson} setPersons={setPersons}/>
     </div>
   )
 }
--- Part 2/phonebook/src/components/Persons.tsx
+++ Part 2/phonebook/src/components/Persons.tsx
@@ -1,11 +1,29 @@
+import phoneService from "../services/Phoneservice"
 
+const Persons = ({persons, searchPerson, setPersons}) => {
+    let filteredPersons = []
 
-const Persons = ({persons}) => {
-    return (
-        persons.map(person => {
-            return <p key={person.id}>{person.name} {person.number}</p>;
-          }) 
-    )
+    const deletePersonFromPhonebook = (id,name) => {
+        const resp = window.confirm(`Delete ${name} ?`)
+        if (resp) {
+            phoneService
+            .deletePerson(id)
+            .then(returnedDeletedPerson => {
+                persons = persons.filter(person => person.id !== returnedDeletedPerson.id)
+                setPersons(persons)
+            })        
+        } 
+    }
+    if (searchPerson.length !== 0) {
+       filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchPerson.toLowerCase()))
+        return (
+            filteredPersons.map(person => <p key={person.id}>{person.name} {person.number} <button onClick={() => deletePersonFromPhonebook(person.id, person.name)}>Delete Person</button></p>) 
+        )
+    } else {
+        return (
+            persons.map(person => <p key={person.id}>{person.name} {person.number} <button onClick={() => deletePersonFromPhonebook(person.id, person.name)}>Delete Person</button></p>) 
+        )
+    }
 }
 
 
